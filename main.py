f474ee282b6bdd7cec34e16de3360bf30f84fa07
Ivan Sushencov <ivan4.03@bk.ru>
2022-11-21 17:50:19 +0300

    Added main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import string
+import random
+ 
+# Getting password length
+length = 11
+ 
+characterList = "" + string.ascii_letters   
+ 
+password = []
+ 
+for i in range(length):
+   
+    # Picking a random character from our
+    # character list
+    randomchar = random.choice(characterList)
+     
+    # appending a random character to password
+    password.append(randomchar)
+ 
+# printing password as a string
+print("The random password is " + "".join(password))
+close=input(" ") 
